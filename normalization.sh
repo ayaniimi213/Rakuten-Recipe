9fa6a92c84f6e2e93653a7c4074cfeec4d1e366e
Ayahiko Niimi <a-niimi@qc4.so-net.ne.jp>
2013-06-09 03:41:41 +0900

    Update normalization.sh

--- normalization.sh
+++ normalization.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-rm recipe_all_20120705_normalized.txt
-rm recipe_material_20120705_normalized.txt
-
 # 変な文字が入っているので、ココだけ切り出して編集
 sed -n 234685p recipe_all_20120705.txt | nkf -w -Z1 --no-best-fit-chars -x > 234685.txt
 sed -n 280174p recipe_all_20120705.txt | nkf -w -Z1 --no-best-fit-chars -x > 280174.txt
