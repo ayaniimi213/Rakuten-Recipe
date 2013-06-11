9bb81bcb57bdb399b50e317affd0d622c88a1184
Ayahiko Niimi <a-niimi@qc4.so-net.ne.jp>
2013-06-11 20:05:14 +0900

    Create query-postgresql.sql

---
+++ query-postgresql.sql
@@ -0,0 +1,71 @@
+# SQL例文(PostgreSQL)
+
+# レシピカテゴリの上位100件
+select "Lcategory", count(*) as count from recipe_all group by "Lcategory" order by count DESC limit 100;
+
+# レシピ登録ユーザの上位20位
+select "userID", count(*) as count from recipe_all group by "userID" order by count DESC limit 20;
+# あるユーザの登録レシピカテゴリ
+select "Lcategory", count(*) as count from recipe_all where "userID" = '1580000101' group by "Lcategory" order by count DESC limit 100;
+select "Lcategory", count(*) as count from recipe_all where "userID" = '1990000160' group by "Lcategory" order by count DESC limit 100;
+select "Lcategory", count(*) as count from recipe_all where "userID" = '1580002407' group by "Lcategory" order by count DESC limit 100;
+
+# よく使われる量の表現
+select volume, count(*) as count from recipe_material group by volume order by count DESC limit 100;
+
+# 使われる食材ランキング
+select material, count(*) as count from recipe_material group by material order by count DESC limit 100;
+# 使われない食材ランキング
+select material, count(*) as count from recipe_material group by material order by count ASC limit 100;
+
+# 中カテゴリ（パスタ）に登録されている小カテゴリ
+select "Scategory", count(*) as count from recipe_all where "Mcategory" = 'パスタ' group by "Scategory" order by count DESC;
+
+# 食材数(重複を除いく)
+select count(distinct material) from recipe_material;
+
+# レシピタイトルと食材、量を表示
+SELECT recipe_all."recipeID", recipe_all."recipeTitle", recipe_material.material, recipe_material.volume
+FROM recipe_all INNER JOIN recipe_material USING ("recipeID")
+ where "Mcategory" = 'パスタ' limit 20;
+
+# レシピタイトルと食材、量を表示（集計表, comma_join()関数を利用）
+SELECT recipe_all."recipeID", recipe_all."recipeTitle", 
+CASE WHEN replace(comma_join(CASE WHEN recipe_material.material ~ '塩' OR recipe_material.material ~ 'しお' THEN recipe_material.volume ELSE '' END), ',', '')  = '適量' THEN 
+'10'
+  WHEN replace(comma_join(CASE WHEN recipe_material.material ~ '塩' OR recipe_material.material ~ 'しお' THEN recipe_material.volume ELSE '' END), ',', '')  != '' THEN 
+		replace(comma_join(CASE WHEN recipe_material.material ~ '塩' OR recipe_material.material ~ 'しお' THEN recipe_material.volume ELSE '' END), ',', '')
+	ELSE '0' END AS "塩",
+CASE WHEN replace(comma_join(CASE WHEN recipe_material.material ~ '胡椒' THEN recipe_material.volume ELSE '' END), ',', '')  != '' THEN 
+		replace(comma_join(CASE WHEN recipe_material.material ~ '胡椒' THEN recipe_material.volume ELSE '' END), ',', '')
+	ELSE '0' END AS "胡椒",
+CASE WHEN replace(comma_join(CASE WHEN recipe_material.material ~ '醤油' THEN recipe_material.volume ELSE '' END), ',', '')  != '' THEN 
+		replace(comma_join(CASE WHEN recipe_material.material ~ '醤油' THEN recipe_material.volume ELSE '' END), ',', '')
+	ELSE '0' END AS "醤油",
+CASE WHEN replace(comma_join(CASE WHEN recipe_material.material ~ '牛乳' THEN recipe_material.volume ELSE '' END), ',', '')  != '' THEN 
+		replace(comma_join(CASE WHEN recipe_material.material ~ '牛乳' THEN recipe_material.volume ELSE '' END), ',', '')
+	ELSE '0' END AS "牛乳",
+CASE WHEN replace(comma_join(CASE WHEN recipe_material.material ~ 'チーズ' THEN recipe_material.volume ELSE '' END), ',', '')  != '' THEN 
+		replace(comma_join(CASE WHEN recipe_material.material ~ 'チーズ' THEN recipe_material.volume ELSE '' END), ',', '')
+	ELSE '0' END AS "チーズ",
+CASE WHEN replace(comma_join(CASE WHEN recipe_material.material ~ '納豆' THEN recipe_material.volume ELSE '' END), ',', '')  != '' THEN 
+		replace(comma_join(CASE WHEN recipe_material.material ~ '納豆' THEN recipe_material.volume ELSE '' END), ',', '')
+	ELSE '0' END AS "納豆",
+CASE WHEN replace(comma_join(CASE WHEN recipe_material.material ~ 'マカロニ' THEN recipe_material.volume ELSE '' END), ',', '')  != '' THEN 
+		replace(comma_join(CASE WHEN recipe_material.material ~ 'マカロニ' THEN recipe_material.volume ELSE '' END), ',', '')
+	ELSE '0' END AS "マカロニ",
+CASE WHEN replace(comma_join(CASE WHEN recipe_material.material ~ 'スパゲティ' THEN recipe_material.volume ELSE '' END), ',', '')  != '' THEN 
+		replace(comma_join(CASE WHEN recipe_material.material ~ 'スパゲティ' THEN recipe_material.volume ELSE '' END), ',', '')
+	ELSE '0' END AS "スパゲティ"
+
+FROM recipe_material INNER JOIN recipe_all USING ("recipeID")
+ where "Mcategory" = 'パスタ' GROUP BY recipe_all."recipeID",recipe_all."recipeTitle"  limit 200;
+
+# レシピタイトルと食材、量を表示（集計表, array_to_string()関数を利用）
+SELECT recipe_all."recipeID", recipe_all."recipeTitle", 
+array_to_string(array_agg(CASE WHEN recipe_material.material ~ '塩' OR recipe_material.material ~ 'しお' THEN recipe_material.volume ELSE '' END), ',')
+
+FROM recipe_material INNER JOIN recipe_all USING ("recipeID")
+ where "Mcategory" = 'パスタ' GROUP BY recipe_all."recipeID",recipe_all."recipeTitle"  limit 200;
+
+
