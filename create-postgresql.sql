9e0b386eb2b170fa80d796cffb4afd8888b89996
Ayahiko Niimi <a-niimi@qc4.so-net.ne.jp>
2013-06-14 11:05:21 +0900

    Update create-postgresql.sql

--- create-postgresql.sql
+++ create-postgresql.sql
@@ -4,32 +4,33 @@
 
 CREATE TABLE recipe_all
 (
-  "recipeID" integer,
+  "recipeID" integer NOT NULL,
   "userID" integer,
   "Lcategory" text,
   "Mcategory" text,
   "Scategory" text,
   "recipeTitle" text,
-  "recipeKiccake" text,
+  "recipeTrigger" text,
   "recipeIntro" text,
-  "recipePict" text,
+  "recipePictureName" text,
   "recipeName" text,
   tag1 text,
   tag2 text,
   tag3 text,
   tag4 text,
-  onepoint text,
+  "pointInfo" text,
   "cookTimeID" text,
-  "DonnatokiID" text,
+  "anyTimeID" text,
   "costID" text,
-  people text,
-  "recipeYMD" text
+  "HowManyPeopleID" text,
+  "recipeDate" text
 )
 WITH (
   OIDS=FALSE
 );
 ALTER TABLE recipe_all
   OWNER TO postgres;
+
 
 
 -- Table: recipe_material
@@ -39,7 +40,7 @@ ALTER TABLE recipe_all
 CREATE TABLE recipe_material
 (
   "recipeID" integer,
-  material text,
+  "materialName" text,
   volume text
 )
 WITH (
@@ -48,6 +49,27 @@ WITH (
 ALTER TABLE recipe_material
   OWNER TO postgres;
 
+
+
+-- Table: recipe_material_gram
+
+-- DROP TABLE recipe_material_gram;
+
+CREATE TABLE recipe_material_gram
+(
+  "recipeID" integer,
+  "materialName" text,
+  volume text,
+  gram integer
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE recipe_material_gram
+  OWNER TO postgres;
+
+
+
 copy recipe_all from 'recipe_all_20120705_normalized.txt';
 copy recipe_all from '234685.txt';
 copy recipe_all from '280174.txt';
@@ -55,7 +77,9 @@ copy recipe_material from 'recipe_material_20120705_normalized.txt';
 
 CREATE INDEX recipe_idx on recipe_all("recipeID");
 CREATE INDEX material_idx on recipe_material("recipeID");
-CREATE INDEX material_idx2 on recipe_material(material);
+CREATE INDEX material_idx2 on recipe_material("materialName");
+CREATE INDEX material_gram_idx on recipe_material_gram("recipeID");
+CREATE INDEX material_gram_idx2 on recipe_material_gram("materialName");
 
 VACUUM VERBOSE ANALYZE;
 
